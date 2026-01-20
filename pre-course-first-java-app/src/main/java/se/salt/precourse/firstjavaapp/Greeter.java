2f4e8ae69e32e30a238b6ef90e064f25ccf9d4c1
Sofie Van Dingenen <sofievandingenen@hotmail.com>
2026-01-20 12:56:53 +0100

    Updated program to ask another input, the start date of the course. Then added calculation to the greeter method to calculate how many days there are left until start date.

--- pre-course-first-java-app/src/main/java/se/salt/precourse/firstjavaapp/Greeter.java
+++ pre-course-first-java-app/src/main/java/se/salt/precourse/firstjavaapp/Greeter.java
@@ -3,10 +3,20 @@ package se.salt.precourse.firstjavaapp;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
 
 public class Greeter {
-  private static String greet(String namePassedIn) {
-    return "Welcome to SALT, " + namePassedIn;
+  private static String greet(String namePassedIn, String date) {
+    LocalDate now = LocalDate.now();
+    LocalDate localDate = LocalDate.parse(date);
+    long daysLeft = ChronoUnit.DAYS.between(now, localDate);
+
+    return "Welcome to SALT, " + namePassedIn +
+            "\n Today it is " + daysLeft + " days left until the course starts" ;
   }
 
   public static void main(String[] args) throws IOException {
@@ -15,7 +25,11 @@ public class Greeter {
     BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
     String name = reader.readLine();
 
-    String greeting = greet(name);
+    System.out.print("When does that course start? ");
+
+    String date = reader.readLine();
+
+    String greeting = greet(name,date);
 
     System.out.println(greeting);
   }
